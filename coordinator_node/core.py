465ce90425b778fae8ca9276ea1d509dfd022d29
lukvmil <millerluke1364@gmail.com>
2025-04-09 13:36:26 +0800

    version bump

--- coordinator_node/core.py
+++ coordinator_node/core.py
@@ -1,4 +1,3 @@
-from rid_lib.ext import Cache
 from rid_lib.types import KoiNetNode, KoiNetEdge
 from koi_net import NodeInterface
 from koi_net.protocol.node import NodeProfile, NodeType, NodeProvides
@@ -7,7 +6,6 @@ from .config import PORT
 
 node = NodeInterface(
     name="coordinator",
-    cache=Cache(".cache"),
     profile=NodeProfile(
         base_url=f"http://127.0.0.1:{PORT}/koi-net",
         node_type=NodeType.FULL,
