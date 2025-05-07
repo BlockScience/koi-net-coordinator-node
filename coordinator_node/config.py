00a082ccaf715f9aa0d4bf6e8ab1710f58667ee1
lukvmil <millerluke1364@gmail.com>
2025-05-07 21:29:14 +0800

    update beta 16

--- coordinator_node/config.py
+++ coordinator_node/config.py
@@ -1,9 +1,9 @@
 from pydantic import Field
 from rid_lib.types import KoiNetNode, KoiNetEdge
-from koi_net.config import Config, KoiNetConfig
+from koi_net.config import NodeConfig, KoiNetConfig
 from koi_net.protocol.node import NodeProfile, NodeProvides, NodeType
 
-class CoordinatorConfig(Config):
+class CoordinatorConfig(NodeConfig):
     koi_net: KoiNetConfig | None = Field(default_factory = lambda:
         KoiNetConfig(
             node_name="coordinator",
