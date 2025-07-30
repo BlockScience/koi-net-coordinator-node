f98aa1ab048ee4d1dd39aa0224d632408fd6689c
lukvmil <millerluke1364@gmail.com>
2025-07-29 20:58:14 -0400

    using updated HandlerContext and factored out identity.bundle

--- coordinator_node/handlers.py
+++ coordinator_node/handlers.py
@@ -1,6 +1,6 @@
 import logging
 from rid_lib.types import KoiNetNode, KoiNetEdge
-from koi_net.processor import ProcessorInterface
+from koi_net.processor.handler_context import HandlerContext
 from koi_net.processor.handler import HandlerType
 from koi_net.processor.knowledge_object import KnowledgeObject
 from koi_net.protocol.event import Event, EventType
@@ -11,8 +11,8 @@ from .core import node
 logger = logging.getLogger(__name__)
 
 
-@node.processor.register_handler(HandlerType.Network, rid_types=[KoiNetNode])
-def handshake_handler(proc: ProcessorInterface, kobj: KnowledgeObject):    
+@node.processor.pipeline.register_handler(HandlerType.Network, rid_types=[KoiNetNode])
+def handshake_handler(ctx: HandlerContext, kobj: KnowledgeObject):    
     logger.info("Handling node handshake")
 
     # only respond if node declares itself as NEW
@@ -20,8 +20,10 @@ def handshake_handler(proc: ProcessorInterface, kobj: KnowledgeObject):
         return
         
     logger.info("Sharing this node's bundle with peer")
-    proc.network.push_event_to(
-        event=Event.from_bundle(EventType.NEW, proc.identity.bundle),
+    # TODO: replace with effector.deref after refactor
+    identity_bundle = ctx.cache.read(ctx.identity.rid)
+    ctx.network.push_event_to(
+        event=Event.from_bundle(EventType.NEW, identity_bundle),
         node=kobj.rid,
         flush=True
     )
@@ -31,9 +33,10 @@ def handshake_handler(proc: ProcessorInterface, kobj: KnowledgeObject):
     
     edge_bundle = generate_edge_bundle(
         source=kobj.rid,
-        target=proc.identity.rid,
+        target=ctx.identity.rid,
         edge_type=EdgeType.WEBHOOK,
         rid_types=[KoiNetNode, KoiNetEdge]
     )
-    proc.handle(rid=edge_bundle.rid, event_type=EventType.FORGET)
-    proc.handle(bundle=edge_bundle)
+        
+    ctx.handle(rid=edge_bundle.rid, event_type=EventType.FORGET)
+    ctx.handle(bundle=edge_bundle)
