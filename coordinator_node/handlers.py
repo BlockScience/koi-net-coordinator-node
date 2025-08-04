a1f365f10736ec91c98fdf8163deee200cc4121e
lukvmil <millerluke1364@gmail.com>
2025-08-04 18:19:19 -0400

    refactoring for koi-net changes

--- coordinator_node/handlers.py
+++ coordinator_node/handlers.py
@@ -1,11 +1,10 @@
 import logging
 from rid_lib.types import KoiNetNode, KoiNetEdge
-from koi_net.processor.handler_context import HandlerContext
+from koi_net.context import HandlerContext
 from koi_net.processor.handler import HandlerType
 from koi_net.processor.knowledge_object import KnowledgeObject
 from koi_net.protocol.event import Event, EventType
-from koi_net.protocol.helpers import generate_edge_bundle
-from koi_net.protocol.edge import EdgeType
+from koi_net.protocol.edge import EdgeType, generate_edge_bundle
 from .core import node
 
 logger = logging.getLogger(__name__)
@@ -20,9 +19,8 @@ def handshake_handler(ctx: HandlerContext, kobj: KnowledgeObject):
         return
         
     logger.info("Sharing this node's bundle with peer")
-    # TODO: replace with effector.deref after refactor
-    identity_bundle = ctx.cache.read(ctx.identity.rid)
-    ctx.network.push_event_to(
+    identity_bundle = ctx.effector.deref(ctx.identity.rid)
+    ctx.event_queue.push_event_to(
         event=Event.from_bundle(EventType.NEW, identity_bundle),
         node=kobj.rid,
         flush=True
--- coordinator_node/server.py
+++ coordinator_node/server.py
@@ -1,7 +1,6 @@
 import logging
-from functools import wraps
 from contextlib import asynccontextmanager
-from fastapi import FastAPI, APIRouter, Depends
+from fastapi import FastAPI, APIRouter
 from koi_net.processor.knowledge_object import KnowledgeSource
 from koi_net.protocol.api_models import (
     PollEvents,
@@ -13,7 +12,7 @@ from koi_net.protocol.api_models import (
     ManifestsPayload,
     BundlesPayload
 )
-from koi_net.protocol.secure_models import SignedEnvelope
+from koi_net.protocol.envelope import SignedEnvelope
 from koi_net.protocol.consts import (
     BROADCAST_EVENTS_PATH,
     POLL_EVENTS_PATH,
@@ -44,48 +43,32 @@ koi_net_router = APIRouter(
 )
 
 
-def envelope_handler(func):
-    @wraps(func)
-    async def wrapper(req: SignedEnvelope, *args, **kwargs) -> SignedEnvelope | None:
-        logger.info("Validating envelope")
-        node.secure.validate_envelope(req)
-        logger.info("Calling endpoint handler")
-        result = await func(req, *args, **kwargs)
-        if result is not None:
-            logger.info("Creating response envelope")
-            return node.secure.create_envelope(
-                payload=result,
-                target=req.source_node
-            )
-    return wrapper
-
-
 @koi_net_router.post(BROADCAST_EVENTS_PATH)
-@envelope_handler
+@node.secure.envelope_handler
 async def broadcast_events(req: SignedEnvelope[EventsPayload]):    
     logger.info(f"Request to {BROADCAST_EVENTS_PATH}, received {len(req.payload.events)} event(s)")
     for event in req.payload.events:
         node.processor.handle(event=event, source=KnowledgeSource.External)
     
 @koi_net_router.post(POLL_EVENTS_PATH)
-@envelope_handler
+@node.secure.envelope_handler
 async def poll_events(req: SignedEnvelope[PollEvents]) -> SignedEnvelope[EventsPayload]:
     logger.info(f"Request to {POLL_EVENTS_PATH}")
-    events = node.network.flush_poll_queue(req.payload.rid)
+    events = node.event_queue.flush_poll_queue(req.payload.rid)
     return EventsPayload(events=events)
 
 @koi_net_router.post(FETCH_RIDS_PATH)
-@envelope_handler
+@node.secure.envelope_handler
 async def fetch_rids(req: SignedEnvelope[FetchRids]) -> SignedEnvelope[RidsPayload]:
     return node.response_handler.fetch_rids(req.payload)
 
 @koi_net_router.post(FETCH_MANIFESTS_PATH)
-@envelope_handler
+@node.secure.envelope_handler
 async def fetch_manifests(req: SignedEnvelope[FetchManifests]) -> SignedEnvelope[ManifestsPayload]:
     return node.response_handler.fetch_manifests(req.payload)
 
 @koi_net_router.post(FETCH_BUNDLES_PATH)
-@envelope_handler
+@node.secure.envelope_handler
 async def fetch_bundles(req: SignedEnvelope[FetchBundles]) -> SignedEnvelope[BundlesPayload]:
     return node.response_handler.fetch_bundles(req.payload)
     
